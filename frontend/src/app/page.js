84d7b6ac2d76c9ae6e5e802caa2ea8292015164d
Kris Selberg <kselberg@princeton.edu>
2023-12-21 10:39:53 -0500

    use logo class in main page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,11 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="page-container">
-      <img
-        className="w-32 my-4"
-        src="/images/ptonlogo.png"
-        alt="Princeton Logo"
-      />
+      <img className="logo" src="/images/ptonlogo.png" alt="Princeton Logo" />
       <h1 className="header mb-6">Princeton Student Search</h1>
       <Search />
       <Link href="/add-student" className="mt-2 button">
