39d322060d8d8be3c063091b8633922a05257f49
Kris Selberg <kselberg@princeton.edu>
2023-12-26 12:43:54 -0500

    added search loading and empty result string

--- frontend/src/app/Search.js
+++ frontend/src/app/Search.js
@@ -5,26 +5,37 @@ import { useState } from "react";
 function Search() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searched, setSearched] = useState(false); // Flag to track if search was performed
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    const response = await fetch("/api/search", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    });
-    if (!response.ok) {
-      throw new Error("API response not OK");
-    }
+    setLoading(true);
+    setSearched(true);
+    try {
+      const response = await fetch("/api/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+      });
 
-    let data = [];
-    if (response.headers.get("content-type")?.includes("application/json")) {
-      data = await response.json();
-    }
+      let data = [];
+      if (
+        response.ok &&
+        response.headers.get("content-type")?.includes("application/json")
+      ) {
+        data = await response.json();
+      }
 
-    setResults(Array.isArray(data) ? data : []);
+      setResults(Array.isArray(data) ? data : []);
+    } catch (error) {
+      console.error("Search error:", error);
+      setResults([]);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -35,7 +46,6 @@ function Search() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search..."
-          // make text color black
           className="input mr-2"
         />
         <button type="submit" className="button">
@@ -43,25 +53,31 @@ function Search() {
         </button>
       </form>
       <div>
-        {results.map((result, index) => (
-          <div key={index} className="card">
-            <h3 className="font-bold text-lg">
-              {result.firstName} {result.lastName}
-            </h3>
-            <p>Email: {result.email}</p>
-            <p>
-              <a
-                href={result.linkedinUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 hover:underline"
-              >
-                LinkedIn
-              </a>
-            </p>
-            <p>{result.description}</p>
-          </div>
-        ))}
+        {loading ? (
+          <p className="text">Loading...</p>
+        ) : results.length > 0 ? (
+          results.map((result, index) => (
+            <div key={index} className="card">
+              <h3 className="font-bold text-lg">
+                {result.firstName} {result.lastName}
+              </h3>
+              <p>Email: {result.email}</p>
+              <p>
+                <a
+                  href={result.linkedinUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-600 hover:underline"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>{result.description}</p>
+            </div>
+          ))
+        ) : searched ? (
+          <p className="text">No results found</p>
+        ) : null}
       </div>
     </div>
   );
